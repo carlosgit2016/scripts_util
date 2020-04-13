3075a9f05c8089602992ec4007dd943786b90e4f
Carlos Gabriel Goiani Flor <carlosggflor@gmail.com>
2020-04-13 14:56:50 -0300

    Update Scope Variables

--- powershell/azuredevops/release/change_release_name.ps1
+++ powershell/azuredevops/release/change_release_name.ps1
@@ -1,19 +1,19 @@
+# Change the arguments to continue:
+$Organization = "<organization>"
+$Project = "<project_name>";
+$ReleaseId = <release_id> # You can get on release url
+$Token = "<PAT>"
+$ReleaseName = "<new_release_name>"
+
 # Alter Release name azure devops
-$url = "https://vsrm.dev.azure.com/{organization}/{project}/_apis/release/releases/{release_id}?api-version=5.1"
-$Token = $personalToken
+$url = "https://vsrm.dev.azure.com/$($Organization)/$($Project)/_apis/release/releases/$($ReleaseId)?api-version=5.1"
 #Encode token to Base64
 $EncodedTokenBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f '', $Token)))
-
 $Headers = @{"Authorization" = "Basic $($EncodedTokenBase64)" }
-
 $pipeline = Invoke-RestMethod -Uri $url -Headers $Headers
-
-Write-Host "Pipeline = $($pipeline | ConvertTo-Json -Depth 100)"
-
+Write-Host "Pipeline = $($pipeline | ConvertTo-Json -Depth 99)"
 # Edit variable pipeline name
-$pipeline.name = "new_release_name"
-
+$pipeline.name = $ReleaseName
 ####****************** update the modified object **************************
 $json = @($pipeline) | ConvertTo-Json -Depth 99
-
 Invoke-RestMethod -Uri $url -Method Put -Body $json -Headers $Headers -ContentType "application/json;charset=utf-8"
