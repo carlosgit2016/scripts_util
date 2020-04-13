a6b01d54d1517fd8a4f6050941b2bdbbd1289884
Carlos Gabriel Goiani Flor <carlosggflor@gmail.com>
2020-04-13 00:55:19 -0300

    Create get_obj_properties.ps1

---
+++ powershell/get_obj_properties.ps1
@@ -0,0 +1,14 @@
+# Get Properties of a Powershell Object
+
+function GetObjProperties {
+    param (
+        $ObjectToPrintProperties
+    )
+
+    $properties = ($ObjectToPrintProperties).psobject.properties;
+    return $properties.ForEach{ Write-Host $_.Name };
+}
+
+# Example to use
+$props GetObjProperties -ObjectToPrintProperties (Get-Service) # Get all properties of the object returned by Get-Service cmdlet 
+Write-Host $props
