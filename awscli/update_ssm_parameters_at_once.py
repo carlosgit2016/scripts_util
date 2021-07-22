1652fc8c83cb8b80823d84bb88960f3d06c8fd4d
carlosgit2016 <carlosggflor@gmail.com>
2021-07-22 16:08:01 -0300

    Improve SSM parameter store updates at once

--- awscli/update_ssm_parameters_at_once.py
+++ awscli/update_ssm_parameters_at_once.py
@@ -4,29 +4,48 @@
 import boto3
 import os
 
-path = '/some/path'
-value = 'some_value'
+PARAMETERS_PATH = '/some/path'
+VALUE_TO_UPDATE = 'some_value'
 
-# Login in first to load your profile or provide other environment variables
-os.environ['AWS_PROFILE'] = "some_profile"
-os.environ['AWS_REGION'] = "us-east-1"
+def run():
+    # Login in first to load your profile or provide other environment variables
+    os.environ['AWS_PROFILE'] = "some_profile"
+    os.environ['AWS_REGION'] = "us-east-1"
+    
+    client = boto3.client("ssm")
+    parameters = []
+    next_token = None
+    
+    
+    while True:
+    
+        kwargs = {
+            'Path':PARAMETERS_PATH,
+            'Recursive':True,
+            'WithDecryption':True
+        }
+    
+        if next_token != None:
+            kwargs.update({'NextToken': next_token})
 
-client = boto3.client("ssm")
+        response = client.get_parameters_by_path(**kwargs)
+        parameters.extend(response['Parameters'])
+        if 'NextToken' not in response:
+            break
+        
+        next_token = response['NextToken']
+    
+    for p in parameters:
+        print(f'Parameter to be updated {p["Name"]}')
+        response = client.put_parameter(
+            Name=p['Name'],
+            Value=VALUE_TO_UPDATE,
+            Overwrite=True
+        )
+    
+        print(response)
 
-response = client.get_parameters_by_path(
-    Path=path,
-    Recursive=True,
-    WithDecryption=True
-)
 
-parameters = response['Parameters']
 
-for p in parameters:
-    print(f'Parameter to be updated {p['Name']}')
-    response = client.put_parameter(
-        Name=p['Name'],
-        Value=value,
-        Overwrite=True
-    )
-
-    print(response)
+if __name__ == "__main__":
+    run()
