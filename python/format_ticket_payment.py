9159bb8a6b92183b2b6031ec72642abe3559096d
carlosgit2016 <carlosggflor@gmail.com>
2021-11-09 19:51:07 -0300

    Added format_payment_ticket personal script

---
+++ python/format_ticket_payment.py
@@ -0,0 +1,12 @@
+import re
+import sys
+
+def format_payment_ticket(ticket_number):
+    formated_ticket_number = ticket_number.replace(".", "")
+    pattern = r"\s+"
+    formated_ticket_number = re.sub(pattern, "", formated_ticket_number)
+    return formated_ticket_number
+
+if __name__ == "__main__":
+    formated_ticket_number = format_payment_ticket(sys.argv[1])
+    print(formated_ticket_number)
