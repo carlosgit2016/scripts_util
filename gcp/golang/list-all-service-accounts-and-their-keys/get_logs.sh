dc1d4a1867ffd49f64fc80133cfe5dbf82028021
carlosgit2016 <carlosggflor@gmail.com>
2023-08-07 09:10:17 -0400

    Loading asdf in case exists

--- gcp/golang/list-all-service-accounts-and-their-keys/get_logs.sh
+++ gcp/golang/list-all-service-accounts-and-their-keys/get_logs.sh
@@ -6,6 +6,11 @@ KEY_NAME="$1"
 SA_EMAIL="$2"
 PROJECT="$3"
 
-/home/cflor/.asdf/shims/gcloud logging read "protoPayload.authenticationInfo.principalEmail=$SA_EMAIL" --project="$PROJECT" --format=json --freshness=30d|  jq -r "[.[] | \
+# Loading asdf in case exists
+if [ -f "$HOME/.asdf/asdf.sh" ]; then
+    source "$HOME/.asdf/asdf.sh"
+fi
+
+gcloud logging read "protoPayload.authenticationInfo.principalEmail=$SA_EMAIL" --project="$PROJECT" --format=json --freshness=30d|  jq -r "[.[] | \
     select(.protoPayload.authenticationInfo.serviceAccountKeyName | contains(\"$KEY_NAME\"))][0] | \
     \"lastAccessData: \(.protoPayload.requestMetadata.requestAttributes.time) who: \(.protoPayload.requestMetadata.callerIp)\""
